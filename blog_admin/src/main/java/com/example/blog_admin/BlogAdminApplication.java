31b7c50c0211dc65462a5d21b1dfc60cf211cf23
moki <1136232183@qq.com>
2024-12-22 15:46:53 +0800

    项目初期构建-nacos

--- blog_admin/src/main/java/com/example/blog_admin/BlogAdminApplication.java
+++ blog_admin/src/main/java/com/example/blog_admin/BlogAdminApplication.java
@@ -2,8 +2,12 @@ package com.example.blog_admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@RefreshScope
 public class BlogAdminApplication {
 
 	public static void main(String[] args) {
